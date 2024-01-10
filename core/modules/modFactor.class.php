462c258c7a25ad2c31202bcb70c6c244dbf4f623
John BOTELLA <john.botella@atm-consulting.fr>
2024-01-10 16:02:14 +0100

    Release 1.3

--- core/modules/modFactor.class.php
+++ core/modules/modFactor.class.php
@@ -60,7 +60,7 @@ class modFactor extends DolibarrModules
 		// Module description, used if translation string 'ModuleXXXDesc' not found (where XXX is value of numeric property 'numero' of module)
 		$this->description = "Gestion de l'affacturage";
 		// Possible values for version are: 'development', 'experimental', 'dolibarr' or version
-		$this->version = '1.2.0';
+		$this->version = '1.3.0';
 		// Key used in llx_const table to save module status enabled/disabled (where MYMODULE is value of property name of module in uppercase)
 		$this->const_name = 'MAIN_MODULE_'.strtoupper($this->name);
 		// Where to store the module in setup page (0=common,1=interface,2=others,3=very specific)
@@ -110,6 +110,10 @@ class modFactor extends DolibarrModules
 		$this->phpmin = array(7,0);					// Minimum version of PHP required by module
 		$this->need_dolibarr_version = array(15, 0);	// Minimum version of Dolibarr required by module
 		$this->langfiles = array("factor@factor");
+
+		// Url to the file with your last numberversion of this module
+		require_once __DIR__ . '/../../class/techatm.class.php';
+		$this->url_last_version = \factor\TechATM::getLastModuleVersionUrl($this);
 
 		// Constants
 		// List of particular constants to add when module is enabled (key, 'chaine', value, desc, visible, 'current' or 'allentities', deleteonunactive)
